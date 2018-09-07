1157ca88647e98ebd5287220b4355cd3149682c8
Ryan Glusic <rglusic@mchenry.edu>
2018-09-07 12:35:58 -0500

    Practice 3.4
    Added my program for the practice 3.4 age classifier assignment.

---
+++ chapter 3/AgeClass.py
@@ -0,0 +1,22 @@
+"""
+    Write a program that asks the user to enter a person's age. The program should display a message indicating
+    whether the person is an infant, a child, a teenager, or an adult.
+
+    Follow these guidelines:
+
+    If the person is 1 year old or less, he or she is an infant.
+    If the person is older than 1 year but younger than 13 years, he or she is a child.
+    If the person is at least 13 years old, but less than 20 years old, he or she is a teenager.
+    If the person is at least 20 years old, he or she is an adult.
+"""
+
+age = int(input("What is your age? "))
+
+if age <= 1:
+    print("You are an infant.")
+elif (age > 1) and (age < 13):
+    print("You are a child.")
+elif (age >= 13) and (age < 20):
+    print("You are a teenager.")
+elif age >= 20:
+    print("You are an adult.")
